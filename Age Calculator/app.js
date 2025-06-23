bb063d60b384379a50dcda486432deb67ef999f2
Amina Saeed <aminasaeed223@gmail.com>
2025-06-23 18:12:10 +0500

    commit

--- Age Calculator/app.js
+++ Age Calculator/app.js
@@ -1,1 +1,11 @@
+let date = document.querySelector('input')
+let btn = document.querySelector('.form button')
+let message = document.querySelector('p')
 
+function calculateAge (){
+let userage = date.value.split('-')
+let calage = 2025-Number(userage[0])
+  message.innerHTML = `your age is ${calage} years old`
+
+}
+btn.addEventListener('click', calculateAge)
--- Currency Converter/script.js
+++ Currency Converter/script.js
@@ -1,5 +1,10 @@
-const URL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@{date}/{apiVersion}/{endpoint}"
+const URL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies"
+
 const dropdown = document.querySelectorAll('.dropdown select')
+const btn = document.querySelector("form button")
+const fromCurr = document.querySelector(".from select")
+const toCurr = document.querySelector(".to select")
+const msg = document.querySelector(".message")
 
 for ( let select of dropdown){
     
@@ -14,14 +19,36 @@ if(select.name === "from" && currCode === "USD"){
     newoption.selected = "Selected"
 }
 }
-select.addEventListener("chnage", (evt) =>{
+select.addEventListener("change", (evt) =>{
     updateFlag(evt.target);
 })
 }
 const updateFlag = (element) =>{
 let currCode = element.value;
 let countrycode = countryList[currCode]
-let newsrc = "https://flagsapi.com/${countrycode}/flat/64.png"
+let newsrc = `https://flagsapi.com/${countrycode}/flat/64.png`
+let img = element.parentElement.querySelector("img");
+img.src = newsrc
+
 }
+btn.addEventListener("click", async (evt)=>{
+    evt.preventDefault()
+    
+    let amount = document.querySelector('form input');
+    let amountval = amount.value;
+    if(amountval === "" || amountval <1){
+        amountval=1;
+        amount.value = 1
+    }
+const newurl = `${URL}/${fromCurr.value.toLowerCase()}.json`;
+
+    let response = await fetch(newurl)
+    let data = await response.json();
+  let rate = data[fromCurr.value.toLowerCase()][toCurr.value.toLowerCase()];
+
+
+    let finalamount = amountval*rate
+   msg.innerText = `${amountval} ${fromCurr.value} = ${finalamount.toFixed(2)} ${toCurr.value}`;
+})
 
 
