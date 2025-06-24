dc523f8bffd39d952940e665a98d37358a0f59eb
Amina Saeed <aminasaeed223@gmail.com>
2025-06-24 12:04:59 +0500

    commit

--- Age Calculator/app.js
+++ Age Calculator/app.js
@@ -5,7 +5,7 @@ let message = document.querySelector('p')
 function calculateAge (){
 let userage = date.value.split('-')
 let calage = 2025-Number(userage[0])
-  message.innerHTML = `your age is ${calage} years old`
+  message.innerHTML = `Your age is ${calage} years old`
 
 }
 btn.addEventListener('click', calculateAge)
